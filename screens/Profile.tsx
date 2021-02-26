9f3dd7d3e2c4e706d4ac6f9ca1d528779b9e32fc
turtlebasket <turtlebasket@gmail.com>
2021-02-26 10:55:44 -0800

    Update profile page with concept

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,5 +1,6 @@
 import { faGripHorizontal } from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';
+import { Image } from 'react-native';
 import {
   SafeAreaView,
   StyleSheet,
@@ -31,21 +32,32 @@ class Profile extends Component {
   render() {
     return (
 
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
 
-        <View style= {styles.card}>
-          <Text style={styles.pageTitle}>Michael Profile (Demo)</Text>
-          <Text style={styles.pageTextGreenBold}>@Michael</Text>
-          <Text style={styles.pageText}>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.</Text>
-        </View>
+        <ScrollView>
+          <View style={styles.cardInvis}>
+            <Image 
+              source={{
+                uri: "https://assets3.thrillist.com/v1/image/1299823/size/tl-horizontal_main/7-weird-stock-images-of-people-struggling-with-basic-cooking"
+              }}
+              style={[styles.profilePictureLarge, {alignSelf: 'center'}]}
+            />
+          </View>
 
-        <View style= {styles.card}>
-          <Text style={styles.pageTitle}>Habits</Text>
-          <Text style={styles.pageText}>Exercise</Text>
-          <Text style={styles.pageText}>Eat</Text>
-          <Text style={styles.pageText}>Sleep</Text>
-        </View>
-      </View>
+          <View style= {styles.card}>
+            <Text style={styles.pageTitle}>Michael Profile (Demo)</Text>
+            <Text style={styles.pageTextGreenBold}>@Michael</Text>
+            <Text style={styles.pageText}>According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.</Text>
+          </View>
+
+          <View style= {styles.card}>
+            <Text style={styles.pageTitle}>Habits</Text>
+            <Text style={styles.pageText}>Exercise</Text>
+            <Text style={styles.pageText}>Eat</Text>
+            <Text style={styles.pageText}>Sleep</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     );
   }
 }
